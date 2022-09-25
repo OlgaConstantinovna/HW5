dd0d969cababcfe962f2ec3f87542a0e5843d161
Olga <princessca.olga@icloud.com>
2022-09-25 14:40:05 +0300

    Home

---
+++ SumElement/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 1)
+sum = sum + numbers[i];
+
+Console.WriteLine($"Сумма не четных элементов = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1,100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+       for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+       Console.WriteLine();
+}
